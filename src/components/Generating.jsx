fd17a36e8139ffd26140a78f684f60f203a3f0d5
bielupc <biel.altimira@estudiantat.upc.edu>
2024-04-01 18:50:46 +0200

    ui changes

--- src/components/Generating.jsx
+++ src/components/Generating.jsx
@@ -3,7 +3,7 @@ import { loading } from "../assets";
 
 function Generating({className}) {
   return (
-    <div className={`flex items-center h-[3.5rem] px-6 bg-n-8/80 rounded-[1.7rem] ${className || ""} text-base font-mon`}>
+    <div className={`flex items-center h-[3.5rem] px-6 bg-n-4/80 rounded-[1.7rem] ${className || ""} text-base font-mon`}>
         <img className='w-5 h-5 mr-4' src={loading} alt="loading"></img>
         Estem treballant en la teva web...
     </div>
